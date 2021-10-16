925b5120eb8cc12cccace5e08ae62993328acff5
Bryanna Vinge <bevinge711@gmail.com>
2021-10-16 10:56:22 -0500

    adding inquirer prompt and beginning of logic

--- server.js
+++ server.js
@@ -17,6 +17,53 @@ const db = mysql.createConnection(
     console.log("Connected to the database")
 );
 
+const toDo = () => {
+    inquirer.prompt({
+        type: "list",
+        name: "actions",
+        message: "What would you like to do?",
+        choices: [
+            "View all Departments",
+            "View all Roles",
+            "View all Employees",
+            "Add a Department",
+            "Add a Role",
+            "Add an Employee",
+            "Update an Employee",
+        ]
+    })
+        .then((choice) => {
+            if ("View all Departments") {
+
+            }
+            if ("View all Roles") {
+
+            }
+            if ("View all Employees") {
+
+            }
+            if ("Add a Department") {
+
+            }
+            if ("Add a Role") {
+
+            }
+            if ("Add an Employee") {
+
+            }
+            if ("Update an Employee") {
+
+            }
+
+        })
+};
+
+//API ROUTES
+//Get - View ALL
+//Post - Add ALL
+//Put - Update Employee
+
+//Functions for each choice
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
