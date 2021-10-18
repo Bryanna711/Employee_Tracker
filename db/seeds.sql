cd7e32cef21c688b593b27f7e98d45292b1c17b4
Bryanna Vinge <bevinge711@gmail.com>
2021-10-18 09:19:32 -0500

    updating seeds data based on class activity

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,25 +1,25 @@
-INSERT INTO department(department_name)
-VALUES("Human Resources"),
-      ("Engineering"),
-      ("Customer Service"),
-      ("Sales");
+INSERT INTO department(id, department_name)
+VALUES(001, "Human Resources"),
+      (002, "Engineering"),
+      (003, "Customer Service"),
+      (004, "Sales");
 
-INSERT INTO role ( title, salary)
-VALUES ("HR Specialist", 75,000),
-       ("Sr Web Developer", 95,000),
-       ("Jr Web Developer", 65,000),
-       ("Customer Service Rep", 45,000),
-       ("Sales Representative", 75,000),
-       ("HR Generalist", 65,000);
+INSERT INTO role (id, title, salary department_id)
+VALUES (001, "HR Specialist", 75,000, 001),
+       (002, "Sr Web Developer", 95,000, 002),
+       (003, "Jr Web Developer", 65,000, 002),
+       (004, "Customer Service Rep", 45,000, 003),
+       (005, "Sales Representative", 75,000, 004),
+       (006, "HR Generalist", 65,000, 001);
 
-INSERT INTO employee (first_name, last_name)
-VALUES ("Sarah", "Golden"),
-       ("Christine", "Relf"),
-       ("Bev", "Hanson"),
-       ("Kate", "Landes"),
-       ("Russ", "Landes"),
-       ("David", "Vinge"),
-       ("Sue", "Dorweiler"),
-       ("Matt", "Goings"),
-       ("Paran", "Kashani"),
-       ("Dillon", "Baxendell");
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (001,"Sarah", "Golden",006, 001),
+       (002,"Christine", "Relf",005,006),
+       (003,"Bev", "Hanson",001, null),
+       (004,"Kate", "Landes",004,006),
+       (005,"Russ", "Landes",002, null),
+       (006,"David", "Vinge",005, null),
+       (007,"Sue", "Dorweiler",005, 006),
+       (008,"Matt", "Goings",004, 006),
+       (009,"Paran", "Kashani",003,005),
+       (010, "Dillon", "Baxendell",003,005);
